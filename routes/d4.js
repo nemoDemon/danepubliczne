03ff8a96b34d7367d7b6658a02149563380c5b81
nemoDemon <235113@stud.umk.pl>
2018-01-26 00:16:15 +0100

    next

--- routes/d4.js
+++ routes/d4.js
@@ -40,7 +40,7 @@ var methods =
 			console.log("Address: " + address);
 			console.log("Latitude: " + latitude);
 			console.log("Longtitude: " + longtitude);
-			var weather = "api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longtitude;
+			var weather = await fetch("api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longtitude);
 			console.log(weather);
 			var returnStr = address + ";" + latitude + ";" + longtitude;
 			return returnStr;
