8c50d45e09d6c2805d089eca19dfc4dd94c07509
nemoDemon <235113@stud.umk.pl>
2018-01-25 12:53:37 +0100

    next

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://user:123@ds113098.mlab.com:13098/danepubliczne');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://user:123@ds113098.mlab.com:13098/danepubliczne');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
