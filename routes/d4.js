2935515eb3cd7ffc1514c9cdbda7aacea336a043
nemoDemon <235113@stud.umk.pl>
2018-01-26 20:19:09 +0100

    commit

--- routes/d4.js
+++ routes/d4.js
@@ -43,9 +43,9 @@ var methods =
 
 			var weatherResponse = await fetch("http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longtitude+"&units=metric&APPID=ec8c7eeb498daedd31d95674436b2f82");
 			var weatherJson = await weatherResponse.json();
-			var sky = weatherJson.weather.main;
-			var temperature = weatherJson.weather.temp;
-			var pressure = weatherJson.weather.pressure;
+			var sky = weatherJson.weather[0].main;
+			var temperature = weatherJson.results[0].temp;
+			var pressure = weatherJson.pressure;
 			console.log(sky);
 			console.log(temperature);
 			console.log(pressure);
